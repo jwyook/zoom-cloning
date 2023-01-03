54f06a2e0c46721307c7f425261ccc45d9652a2b
Yook JIWON <jwyook27@naver.com>
2023-01-03 16:39:10 +0900

    #07 Nicknames

--- src/server.js
+++ src/server.js
@@ -26,10 +26,21 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "None";
   console.log("Connected to Browser");
   socket.on("close", onSocketClose);
-  socket.on("message", (message) => {
-    sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
   });
 });
 
