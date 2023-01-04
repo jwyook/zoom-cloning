a59c04560cca56584986285fde6dfd9bc9a1d0cc
Yook JIWON <jwyook27@naver.com>
2023-01-04 20:52:58 +0900

    #13 Nicknames

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import http from "http";
 import { Server } from "socket.io";
 import path from "path";
 import express from "express";
+import { nextTick } from "process";
 
 const app = express();
 const __dirname = path.resolve();
@@ -21,21 +22,25 @@ const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
 wsServer.on("connection", (socket) => {
+  socket["nickname"] = "Ghost";
   socket.onAny((event) => {
     console.log(`Socket Event:${event}`);
   });
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome");
+    socket.to(roomName).emit("welcome", socket.nickname);
   });
   socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickname)
+    );
   });
   socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", msg);
+    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
     done();
   });
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
 httpServer.listen(3000, handleListen);
